5fdc30f8f94b89df3491957e80e18521169a1b40
karamaru <38310693+karamaru-alpha@users.noreply.github.com>
2022-07-03 17:21:07 +0900

    DB分け (#15)
    * a
    
    * a
    
    * a
    
    * a
    
    * a

--- sql/init.sh
+++ sql/init.sh
@@ -2,12 +2,13 @@
 set -xu -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
-export MYSQL_PORT=${MYSQL_PORT:-3306}
+#export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
+export MYSQL_HOST="172.31.32.112"
+export MYSQL_PORT=3306
 export MYSQL_USER=${MYSQL_USER:-isucon}
 export MYSQL_DBNAME=${MYSQL_DBNAME:-isucondition}
 export MYSQL_PWD=${MYSQL_PASS:-isucon}
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
-cat 0_Schema.sql 1_InitData.sql 2_Patch.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 0_Schema.sql 1_InitData.sql 2_Patch.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER -pisucon $MYSQL_DBNAME
