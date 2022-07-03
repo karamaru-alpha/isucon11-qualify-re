6fc6829b931b169ce96db54115f04301b371d60d
karamaru <mrnk3078@gmail.com>
2022-07-03 17:12:52 +0900

    a

--- sql/init.sh
+++ sql/init.sh
@@ -4,7 +4,7 @@ set -xu -o pipefail
 CURRENT_DIR=$(cd $(dirname $0);pwd)
 #export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
 export MYSQL_HOST=${MYSQL_HOST:-172.31.32.112}
-export MYSQL_PORT=${MYSQL_PORT:-3306}
+export MYSQL_PORT=3306
 export MYSQL_USER=${MYSQL_USER:-isucon}
 export MYSQL_DBNAME=${MYSQL_DBNAME:-isucondition}
 export MYSQL_PWD=${MYSQL_PASS:-isucon}
